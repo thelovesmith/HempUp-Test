c2dccb7edc2887e473fc773d5c36b731ee7a5891
thelovesmith <averydante9@gmail.com>
2021-08-12 19:42:28 -0400

    updated react springs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React from "react";
 import { useSpring, animated } from "react-spring";
-import './App.css';
+import "./App.css";
 
 function App() {
-  const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.1]
-  const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
+  const calc = (x, y) => [
+    -(y - window.innerHeight / 2) / 20,
+    (x - window.innerWidth / 2) / 20,
+    1.1,
+  ];
+  const trans = (x, y, s) =>
+    `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
   console.log(`tis' working`);
   // const props = useSpring({ opactiy: 1, from: { opactiy: 0 } });
 
   function Card() {
-    const [props, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } }))
+    const [props, set] = useSpring(() => ({
+      xys: [0, 0, 1],
+      config: { mass: 5, tension: 350, friction: 40 },
+    }));
     return (
       <animated.div
         className="card"
@@ -17,19 +25,17 @@ function App() {
         onMouseLeave={() => set({ xys: [0, 0, 1] })}
         style={{ transform: props.xys.interpolate(trans) }}
       />
-    )
+    );
   }
   return (
-
-
-    <>
-    <div className="overlay" ></div>
+    <React.Fragment>
+      <div className="overlay"></div>
       <Card>
         {/* <div id='test-div'>
         HEYYYYYYYYY
-        </div> */}
-        </Card>
-    </>
+      </div> */}
+      </Card>
+    </React.Fragment>
   );
 }
 
